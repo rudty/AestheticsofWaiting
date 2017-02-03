13e96c565de5715a36a144efdb8d10b57a50b56c
rudty <rudtyz@gmail.com>
2017-02-03 22:47:55 +0900

    init

---
+++ app/src/main/java/org/waiting/aestheticsofwaiting/AppApplication.java
@@ -0,0 +1,19 @@
+package org.waiting.aestheticsofwaiting;
+
+import android.app.Application;
+
+import com.tsengvn.typekit.Typekit;
+
+/**
+ * Created by d on 2017-02-02.
+ * 폰트용 동영상
+ */
+
+public class AppApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Typekit.getInstance()
+                .addNormal(Typekit.createFromAsset(this, "MY_FONT"));
+    }
+}
